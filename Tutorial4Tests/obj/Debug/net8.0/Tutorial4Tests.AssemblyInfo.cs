cb5d30e9145e99151ed23ba4eff436750d93629b
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-03-30 17:05:47 +0200

    end of class progress

--- Tutorial4Tests/obj/Debug/net8.0/Tutorial4Tests.AssemblyInfo.cs
+++ Tutorial4Tests/obj/Debug/net8.0/Tutorial4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f75d9edf6a5bb4835ca231d783fda3a847d7db4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
